5822db51f82b5e696043c2620a62bd8f4934901b
Byongyun Choi/최병윤 <87302775+ByongyunChoi@users.noreply.github.com>
2022-01-24 21:59:25 +0900

    Home 화면 개발중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 import { signOut } from "firebase/auth";
 import { authService } from "../fbase";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, doc, onSnapshot } from "firebase/firestore";
 
 function Home() {
   const [loginUserEmail, setLoginUserEmail] = useState("");
+  const db = getFirestore();
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
@@ -14,10 +24,36 @@ function Home() {
         setLoginUserEmail("");
       }
     });
+
+    console.log("Current 1111111");
+
+    const unsub = onSnapshot(
+      doc(db, "commis", "VfP11FhrEW1HnzYes5GJ"),
+      (doc) => {
+        console.log("Current data: ", doc.data());
+      }
+    );
   }, []);
 
   const onLogoutClick = (event) => {
+    console.log("onLogoutClick");
+    console.log("authService.uid :  ", authService.currentUser.uid);
     signOut(authService);
+  };
+
+  const onAddClick = async (event) => {
+    try {
+      console.log("authService.uid :  ", authService.currentUser.uid);
+      const docRef = await addDoc(collection(db, "commis"), {
+        uid: authService.currentUser.uid,
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   };
 
   return (
@@ -26,6 +62,9 @@ function Home() {
       <h2>{loginUserEmail}</h2>
       <Button type="submit" variant="contained" onClick={onLogoutClick}>
         Logout
+      </Button>
+      <Button variant="contained" onClick={onAddClick}>
+        Add
       </Button>
     </div>
   );
